bacde8bbe5c1db6f9a504ea384d24998db30ede2
Felipe Augusto <felipe.93nt@gmail.com>
2021-06-15 11:17:54 -0300

    adicionando cor definitiva ao footer + adição de footer a home

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  twitterurl: string = 'https://twitter.com/F93nt'
+  linkedinurl: string =  'https://www.linkedin.com/in/felipe93nt'
+  githuburl: string = 'https://github.com/felipe93nt'
+
+  twtterimg: string = './assets/images/twitter.png'
+  linkedinimg: string = './assets/images/linkedin.png'
+  githubimg: string = './assets/images/github-icon.png'
+
   constructor() { }
 
   ngOnInit(): void {
